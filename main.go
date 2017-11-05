cb533f2527671021b876d6999aa6cfa0d208acc1
Henrique Lopes <riquellopes@gmail.com>
2017-11-05 03:16:17 -0200

    implementing web service

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT should be set.")
+	}
+
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		io.WriteString(res, "Hello World.")
+	})
+
+	http.ListenAndServe(":"+port, nil)
 }
